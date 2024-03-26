f23b5eb467b6ac467e8e97cea63561282bd6e92c
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-03-26 12:12:33 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,71 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      //同ファイルからのエクスポートは警告
+      'warn',
+    ],
+    'react/function-component-definition': [
+      //関数コンポーネントはアロー関数で定義
+      'error',
+      { namedComponents: 'arrow-function' },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error', //any禁止
+    'no-console': [
+      //consoleは警告 ただし、warnとerrorは許可
+      'warn',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        //アロー関数はPascalCase
+        selector: 'variable',
+        format: ['StrictPascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        //boolean型の変数とboolean型を返す関数は接頭語指定
+        selector: ['variable', 'function'],
+        types: ['boolean'],
+        format: ['StrictPascalCase'],
+        prefix: ['is', 'has'],
+      },
+      {
+        selector: 'function',
+        format: ['strictCamelCase', 'StrictPascalCase'],
+      },
+      {
+        selector: ['variable', 'parameter', 'method', 'property', 'typeParameter'],
+        format: ['strictCamelCase'],
+      },
+      {
+        selector: ['class', 'interface', 'typeAlias'],
+        format: ['StrictPascalCase'],
+      },
+    ],
+  },
+};
