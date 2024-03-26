145fe62b397b523f93b984d4f6af024a472c5cf2
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-03-26 15:16:54 +0900

    [fix] カスタムスタイルを最優先に
    module.cssによってスタイルを指定しても、MUIが優先されるのを防ぐため

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from './Router';
+import { StyledEngineProvider } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <StyledEngineProvider injectFirst>
+      <RouterProvider router={Router} />
+    </StyledEngineProvider>
   </React.StrictMode>,
 );
