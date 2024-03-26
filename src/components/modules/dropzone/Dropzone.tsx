babe8367b60322e4bcfad246da886dffb4b1fc3d
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-03-26 17:03:48 +0900

    [add]Dropzoneコンポーネントを追加

---
+++ src/components/modules/dropzone/Dropzone.tsx
@@ -0,0 +1,36 @@
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import styles from './dropzone.module.css';
+import { Box } from '@mui/material';
+import { ViewImage } from './Image';
+
+export const Dropzone = (props: {
+  img: string | undefined;
+  setImg: React.Dispatch<React.SetStateAction<string | undefined>>;
+}) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+
+    const reader = new FileReader();
+    reader.readAsDataURL(acceptedFiles[0]);
+    reader.onload = () => {
+      if (typeof reader.result != "string") return;
+      props.setImg(reader.result);
+    };
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'image/png': ['.png', '.jpg', '.jpeg'],
+    }, onDrop
+  });
+
+  return (
+    <Box className={styles.dropzone}>
+      <Box  {...getRootProps()} className={styles.dropzone_main}>
+        <input {...getInputProps()} />
+        {props.img === undefined && <p>a</p>}
+        {props.img != undefined && <ViewImage img={props.img} />}
+      </Box>
+    </Box>
+  );
+}
--- src/pages/upload/Upload.tsx
+++ src/pages/upload/Upload.tsx
@@ -1,7 +1,12 @@
+import { Dropzone } from "@/components/modules/dropzone/Dropzone";
+import { useState } from "react";
+
 export const UploadPage = () => {
+  const [img, setImg] = useState<string | undefined>(undefined);
+
   return (
     <div>
-      <h1>Upload Page</h1>
+      <Dropzone img={img} setImg={setImg} />
     </div>
   );
 };
