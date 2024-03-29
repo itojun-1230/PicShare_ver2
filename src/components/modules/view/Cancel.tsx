8cabae618cc475f9baaf623e6125ba606f8ec423
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-03-29 13:03:12 +0900

    [update] cancel時にidもリセット

--- src/components/modules/view/Cancel.tsx
+++ src/components/modules/view/Cancel.tsx
@@ -2,10 +2,14 @@ import { IconButton } from '@mui/material';
 import CancelIcon from '@mui/icons-material/Cancel';
 import styles from './view.module.css';
 
-export const Cancel = (props: { setImg: React.Dispatch<React.SetStateAction<string>> }) => {
+export const Cancel = (props: { 
+  setId: React.Dispatch<React.SetStateAction<string>>;
+  setImg: React.Dispatch<React.SetStateAction<string>>;
+ }) => {
   const CancelClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent> | React.TouchEvent<HTMLButtonElement>) => {
     e.stopPropagation();
     props.setImg("");
+    props.setId("");
   };
 
   return (
