84b6966bb5cb705b51f61d7b799333d0a13a3fa7
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-03-29 12:29:58 +0900

    [add] img

--- src/components/modules/view/ViewBox.tsx
+++ src/components/modules/view/ViewBox.tsx
@@ -2,13 +2,22 @@ import { Box } from "@mui/material";
 import styles from "./view.module.css";
 import { useState } from "react";
 import { InputMenu } from "./InputMenu";
+import { Cancel } from "./Cancel";
 
 export const ViewBox = () => {
   const [img, setImg] = useState<string>("");
 
   return (
     <Box className={styles.viewbox}>
-      <InputMenu setImg={setImg} />
+      {img && (
+        <>
+          <img src={img} alt="画像" />
+          <Cancel setImg={setImg} />
+        </>
+      )}
+      {!img && (
+        <InputMenu setImg={setImg} />
+      )}
     </Box>
   )
 }
